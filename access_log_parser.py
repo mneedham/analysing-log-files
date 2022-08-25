4c82d3028d241f6ad0d5061bf0ecf0856dd8e4a2
Mark Needham <m.h.needham@gmail.com>
2022-08-25 06:57:45 +0100

    updates

--- access_log_parser.py
+++ access_log_parser.py
@@ -4,22 +4,34 @@ import json
 from apachelogs import LogParser
 import httpagentparser
 
-log_parser = LogParser("%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"")
-
-old_new = {
-    "%>s": "status",
-    "%h": "remoteHostName",
-    "%b": "responseBytes",
-    "%t": "ts",
-    "Referer": "referer",
-    "User-Agent": "userAgent"
-}
-
-fields_to_drop = ["%l", "%u", "%{Referer}i", "%{User-Agent}i", "%r"]
-
 class AccessLogParser:
+    OLD_NEW = {
+        "%>s": "status",
+        "%h": "remoteHostName",
+        "%b": "responseBytes",
+        "%t": "ts",
+        "Referer": "referer",
+        "User-Agent": "userAgent"
+    }
+
+    FIELDS_TO_DROP = ["%l", "%u", "%{Referer}i", "%{User-Agent}i", "%r"]
+
     def __init__(self):
         self.log_parser = LogParser("%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"") 
+
+    def extract_url(self, request_parts):
+        return request_parts.split(" ")[1]
+
+    def rename_properties(self, row):
+        for old, new in self.OLD_NEW.items():
+            row[new] = row.pop(old)
+
+    def drop_fields(self, row):
+        for key in self.FIELDS_TO_DROP:
+            row.pop(key)
+
+    def extract_user_agent(self, row):
+      return httpagentparser.detect(row["userAgent"])
 
     def parse(self, event):
         output = self.log_parser.parse(event)
@@ -30,15 +42,14 @@ class AccessLogParser:
             **output.headers_in,
             **output.directives
         }
-        row["url"] = row["%r"].split(" ")[1]
+        row["url"] = self.extract_url(row["%r"])
 
-        for old, new in old_new.items():
-            row[new] = row.pop(old)
+        self.rename_properties(row)
+        self.drop_fields(row)
 
-        for key in fields_to_drop:
-            row.pop(key)    
+        user_agent_parts = self.extract_user_agent(row)
 
-        return {**row, **httpagentparser.detect(row["userAgent"])}
+        return {**row, **user_agent_parts}
 
 if __name__ == "__main__":
     access_log_parser = AccessLogParser()
