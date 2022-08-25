5af0d0d71749138247dc621f5ed92d3e32f59b17
Mark Needham <m.h.needham@gmail.com>
2022-08-25 09:40:10 +0100

    init

--- access_log_parser_complete.py
+++ access_log_parser_complete.py
@@ -2,34 +2,12 @@ import sys
 import json
 
 from apachelogs import LogParser
+
 import httpagentparser
 
 class AccessLogParser:
     def __init__(self):
-        self.log_parser = LogParser("%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"") 
-
-    def extract_url(self, request_parts):
-        return request_parts.split(" ")[1]
-
-    OLD_NEW = {
-        "%>s": "status",
-        "%h": "remoteHostName",
-        "%b": "responseBytes",
-        "%t": "ts",
-        "Referer": "referer",
-        "User-Agent": "userAgent"
-    }
-    def rename_properties(self, row):
-        for old, new in self.OLD_NEW.items():
-            row[new] = row.pop(old)
-
-    FIELDS_TO_DROP = ["%l", "%u", "%{Referer}i", "%{User-Agent}i", "%r"]
-    def drop_fields(self, row):
-        for key in self.FIELDS_TO_DROP:
-            row.pop(key)
-
-    def extract_user_agent(self, row):
-      return httpagentparser.detect(row["userAgent"])
+        self.log_parser = LogParser("%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"")
 
     def parse(self, event):
         output = self.log_parser.parse(event)
@@ -40,18 +18,38 @@ class AccessLogParser:
             **output.headers_in,
             **output.directives
         }
-        row["url"] = self.extract_url(row["%r"])
 
-        self.rename_properties(row)
+        method, url, protocol = row["%r"].split(" ")
+        row |= {"method": method, "url": url, "protocol": protocol}
+
+        user_agent_parts = httpagentparser.detect(row["User-Agent"])
+        row |= user_agent_parts
+
         self.drop_fields(row)
+        self.rename_fields(row)
 
-        user_agent_parts = self.extract_user_agent(row)
+        return row
 
-        return {**row, **user_agent_parts}
+    FIELDS_TO_DROP = ["%l", "%u", "%{Referer}i", "%{User-Agent}i", "%r"]
+    def drop_fields(self, row):
+        for key in self.FIELDS_TO_DROP:
+            row.pop(key)
+
+    OLD_NEW = {
+        "%>s": "status",
+        "%h": "remoteHostName",
+        "%b": "responseBytes",
+        "%t": "ts",
+        "Referer": "referer",
+        "User-Agent": "userAgent"
+    }
+    def rename_fields(self, row):
+        for old, new in self.OLD_NEW.items():
+            row[new] = row.pop(old)
 
 if __name__ == "__main__":
     access_log_parser = AccessLogParser()
     for line in sys.stdin:
-        data = json.loads(line)
-        parsed_message = access_log_parser.parse(data)    
+        message = json.loads(line)
+        parsed_message = access_log_parser.parse(message)    
         print(json.dumps(parsed_message, default=str))
